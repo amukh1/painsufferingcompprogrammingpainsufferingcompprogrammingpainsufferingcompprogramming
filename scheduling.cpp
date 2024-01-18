c04d871ae60c41453575965cc7a950889715cd6a
Amukh1 <85003861+amukh1@users.noreply.github.com>
2024-01-18 20:45:06 +0000

    .

---
+++ scheduling.cpp
@@ -0,0 +1,16 @@
+#include "bits/stdc++.h"
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    std::vector<std::tuple<int,int>> times;
+
+    for(int i = 0; i<n; i++) {
+        int a,b;
+        std::cin >> a;
+        std::cin >> b;
+
+        std::tuple<int,int> x = std::make_tuple(a,b);
+    }
+}
